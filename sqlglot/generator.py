e878d9cbf366704d8a6b74b78454bc00105766c6
Joe Bylund <joseph.bylund@gmail.com>
2026-09-15 13:26:59 -0400

    fix(generator): carry an offset out to the derived table a set operation nests into [CLAUDE]

--- sqlglot/generator.py
+++ sqlglot/generator.py
@@ -1902,8 +1902,9 @@ class Generator:
         if not self.SET_OP_MODIFIERS:
             limit = expression.args.get("limit")
             order = expression.args.get("order")
+            offset = expression.args.get("offset")
 
-            if limit or order:
+            if limit or order or offset:
                 select = self._move_ctes_to_top_level(
                     exp.subquery(expression, "_l_0", copy=False).select("*", copy=False)
                 )
@@ -1912,6 +1913,8 @@ class Generator:
                     select = select.limit(limit.pop(), copy=False)
                 if order:
                     select = select.order_by(order.pop(), copy=False)
+                if offset:
+                    select = select.offset(offset.pop(), copy=False)
                 return self.sql(select)
 
         sqls: list[str] = []
